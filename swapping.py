9654e116fa7b16068382b0b4f3b8a6b5ee6577ad
Sahana0206 <158017043+Sahana0206@users.noreply.github.com>
2024-01-29 11:15:36 +0530

    Add files via upload

---
+++ swapping.py
@@ -0,0 +1,11 @@
+you = 10
+we = 20
+print("before swapping:")
+print("you =", you)
+print("we =", we)
+you = you + we
+we = you - we
+you = you - we
+print("after swapping:")
+print("you =", you)
+print("we =", we)
